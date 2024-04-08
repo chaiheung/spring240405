5b70861b2bae8db739efa89f86dddb7f64eab4fc
chaiheung <chaiheung@naver.com>
2024-04-08 15:27:42 +0900

    com.study.controller.Controller08

---
+++ src/main/java/com/study/controller/Controller08.java
@@ -0,0 +1,73 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("main8")
+public class Controller08 {
+    @RequestMapping("sub1")
+    public void method1(Model model) {
+        model.addAttribute("name1", "value1");
+        model.addAttribute("name2", "value2");
+    }
+
+    @RequestMapping("sub2")
+    public void method2(Model model) {
+        model.addAttribute("city", "seoul");
+        model.addAttribute("address", "sinchon");
+    }
+
+    @RequestMapping("sub3")
+    public void method3(Model model) {
+        model.addAttribute("name", "bori");
+        model.addAttribute("age", 10);
+    }
+
+    @RequestMapping("sub4")
+    public void method4(Model model) {
+        // model attribute의 name : String
+        // model attribute의 value : Object
+
+        // 자주 사용하는 타입
+        // String, 기본타입(Wrapper)
+        // Arrays, List, Map
+        // JavaBeans
+
+        // Arrays
+        model.addAttribute("car", new String[]{"tesla", "kia", "hyundai"});
+
+    }
+
+    @RequestMapping("sub5")
+    public void method5(Model model) {
+        model.addAttribute("cities", new String[]{"seoul", "daegu", "busan"});
+        model.addAttribute("countries", new String[]{"korea", "usa", "kenya"});
+    }
+
+    @RequestMapping("sub6")
+    public void method6(Model model) {
+        List<String> data = List.of("java", "css", "html", "jsp");
+        model.addAttribute("myList", data);
+
+        // /main8/sub6.jsp
+    }
+
+    @RequestMapping("sub7")
+    public void method7(Model model) {
+        List<String> data = List.of("😂", "😎", "😍");
+        model.addAttribute("myList", data);
+
+        List<Integer> numbers = List.of(10, 5, 0);
+        model.addAttribute("yourList", numbers);
+    }
+
+    @RequestMapping("sub8")
+    public void method8(Model model) {
+        model.addAttribute("numbers", List.of(500.0, 3.14));
+        model.addAttribute("cities", List.of("서울", "런던", "뉴욕"));
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/main8/sub1.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>/main8/sub1.jsp 입니다.</h1>
+<%--jsp 주석--%>
+<%--${attribute명}을 작성하면 매핑된 해당 attribute value가 출력됨 --%>
+<h1>${name1}</h1>
+<h1>컨트롤러에서 전달해준 값은 ${name1}입니다.</h1>
+<h1>컨트롤러에서 전달해준 또 다른 값은 ${name2}입니다.</h1>
+</body>
+</html>
