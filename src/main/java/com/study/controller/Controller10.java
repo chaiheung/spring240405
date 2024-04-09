6c777e639b488e439a8270c092cc1fbe09b1eb21
chaiheung <chaiheung@naver.com>
2024-04-09 12:10:15 +0900

    com.study.controller.Controller10/sub6~9

--- src/main/java/com/study/controller/Controller10.java
+++ src/main/java/com/study/controller/Controller10.java
@@ -1,5 +1,9 @@
 package com.study.controller;
 
+import com.study.domain.MyBean10A;
+import com.study.domain.MyBean10B;
+import com.study.domain.MyBean10C;
+import com.study.domain.MyBean10D;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -73,5 +77,100 @@ public class Controller10 {
         model.addAttribute("data", "값 1");
         model.addAttribute("데이타", "값 2");
         model.addAttribute("3", "삼");
+    }
+
+    @RequestMapping("sub6")
+    public void method6(Model model) {
+        var data = new ArrayList<MyBean10A>();
+        MyBean10A e1 = new MyBean10A();
+        MyBean10A e2 = new MyBean10A();
+        MyBean10A e3 = new MyBean10A();
+
+        e1.setName("son");
+        e1.setAge(32);
+        e1.setAddress("london");
+
+        e2.setName("lee");
+        e2.setAge(23);
+        e2.setAddress("paris");
+
+        e3.setName("kim");
+        e3.setAge(28);
+        e3.setAddress("munich");
+
+        data.add(e1);
+        data.add(e2);
+        data.add(e3);
+
+        model.addAttribute("myList", data);
+    }
+
+    @RequestMapping("sub7")
+    public void method7(Model model) {
+        var list = new ArrayList<MyBean10B>();
+        MyBean10B e1 = new MyBean10B();
+        MyBean10B e2 = new MyBean10B();
+        MyBean10B e3 = new MyBean10B();
+
+        e1.setModelName("ferrari");
+        e1.setPrice(10_000.00);
+        e1.setColor("red");
+
+        e2.setModelName("lamborghini");
+        e2.setPrice(9_000.00);
+        e2.setColor("yellow");
+
+        e3.setModelName("porsche");
+        e3.setPrice(8_000.00);
+        e3.setColor("green");
+
+        list.add(e1);
+        list.add(e2);
+        list.add(e3);
+
+        model.addAttribute("cars", list);
+    }
+
+    @RequestMapping("sub8")
+    public void method8(Model model) {
+        var list = new ArrayList<MyBean10C>();
+
+        MyBean10C e1 = new MyBean10C();
+        MyBean10C e2 = new MyBean10C();
+
+        // ${myList[0].name}
+        e1.setName("한국");
+        // ${myList[0].cities[0]}
+        // ${myList[0].cities[1]}
+        // ${myList[0].cities[2]}
+        e1.setCities(List.of("서울", "인천", "부산"));
+
+        // ${myList[1].name}
+        e2.setName("미국");
+        // ${myList[1].cities[0]}
+        // ${myList[1].cities[1]}
+        e2.setCities(List.of("뉴욕", "LA"));
+
+        list.add(e1);
+        list.add(e2);
+
+        model.addAttribute("myList", list);
+    }
+
+    @RequestMapping("sub9")
+    public void method9(Model model) {
+        var data = new ArrayList<MyBean10D>();
+        MyBean10D e1 = new MyBean10D();
+        MyBean10D e2 = new MyBean10D();
+
+
+        e1.setCity("seoul");
+        e1.setFoods(List.of("김치", "제육", "삼겹살"));
+
+        e2.setCity("london");
+        e2.setFoods(List.of("피쉬", "칩스", "민트초코"));
+
+        var obj = List.of(e1, e2);
+        model.addAttribute("data", obj);
     }
 }
---
+++ src/main/java/com/study/domain/MyBean10B.java
@@ -0,0 +1,31 @@
+package com.study.domain;
+
+public class MyBean10B {
+    private String modelName;
+    private Double price;
+    private String color;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
