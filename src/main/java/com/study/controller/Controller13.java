b0a1672de922796117d2a5bf3f5a2d19bf6e37bf
chaiheung <chaiheung@naver.com>
2024-04-11 12:41:08 +0900

    com.study.controller.Controller13. sub1~sub2

---
+++ src/main/java/com/study/controller/Controller13.java
@@ -0,0 +1,21 @@
+package com.study.controller;
+
+import com.study.domain.MyBean13A;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main13")
+public class Controller13 {
+    @RequestMapping("sub1")
+    public void sub1(Model model) {
+        MyBean13A obj1 = new MyBean13A("신촌", "서울", 44);
+        model.addAttribute("obj1", obj1);
+    }
+
+    @RequestMapping("sub2")
+    public void sub2(Model model) {
+
+    }
+}
