24a6d0a21b20e7e6d493675d5213fb29b36b9fda
chaiheung <chaiheung@naver.com>
2024-04-17 16:45:23 +0900

    05like.sql

--- sql/05like.sql
+++ sql/05like.sql
@@ -1,4 +1,4 @@
-USE w3schools
+USE w3schools;
 
 # LIKE : 와일드카드로 조회
 # 와일드카드
@@ -6,37 +6,46 @@ USE w3schools
 # % : 0개 이상의 글자 아무거나
 
 # Cha 이후 아무글자 두개
-SELECT * FROM Products
+SELECT *
+FROM Products
 WHERE ProductName Like 'cha__';
 
-SELECT * FROM Orders
+SELECT *
+FROM Orders
 WHERE OrderDate LIKE '1996-07-0_';
 
-SELECT * FROM Orders
+SELECT *
+FROM Orders
 WHERE OrderDate LIKE '____-07-__';
 
-SELECT * FROM Products
+SELECT *
+FROM Products
 WHERE ProductName LIKE 'cha%';
 
-SELECT * FROM Products
+SELECT *
+FROM Products
 WHERE ProductName LIKE '%ng';
 
-SELECT * FROM Products
+SELECT *
+FROM Products
 WHERE ProductName LIKE 's%s';
 
-SELECT * FROM Products
+SELECT *
+FROM Products
 WHERE ProductName LIKE '%od%';
 
-SELECT * FROM Products
+SELECT *
+FROM Products
 WHERE ProductName LIKE '%anton%';
 
-SELECT * FROM Customers
+SELECT *
+FROM Customers
 WHERE CustomerName LIKE 'b%';
 
-SELECT * FROM Customers
+SELECT *
+FROM Customers
 WHERE Country LIKE '%a';
 
-SELECT * FROM Orders
+SELECT *
+FROM Orders
 WHERE OrderDate LIKE '%01';
-
-SELECT * FROM Products
