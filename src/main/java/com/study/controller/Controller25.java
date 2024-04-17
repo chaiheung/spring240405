9af8c89bead39a76904f9e5ca820c18dd6c33f21
chaiheung <chaiheung@naver.com>
2024-04-17 10:33:05 +0900

    Controller25.sub1

---
+++ src/main/java/com/study/controller/Controller25.java
@@ -0,0 +1,52 @@
+package com.study.controller;
+
+import com.study.domain.MyBean25A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("main25")
+public class Controller25 {
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("sub1")
+    public void method1(@RequestParam("name") String searchName, Model model) throws SQLException {
+        ArrayList<MyBean25A> list = new ArrayList<>();
+        String sql = STR."""
+                SELECT *
+                FROM Employees
+                WHERE LastName = '\{searchName}'
+                """;
+        Connection conn = dataSource.getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        try (rs; stmt; conn) {
+            while (rs.next()) {
+                String id = rs.getString(1);
+                String lastName = rs.getString(2);
+                String firstName = rs.getString(3);
+                String birthDate = rs.getString(4);
+                String photo = rs.getString(5);
+                String notes = rs.getString(6);
+
+                MyBean25A obj = new MyBean25A(id, lastName, firstName,
+                        birthDate, photo, notes);
+                list.add(obj);
+            }
+            model.addAttribute("employees", list);
+        }
+    }
+
+}
