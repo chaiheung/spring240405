35d50236b690cf3f1d288a9e67a812cc79d7ef59
chaiheung <chaiheung@naver.com>
2024-04-18 12:11:39 +0900

    08in.sql

---
+++ sql/08in.sql
@@ -0,0 +1,23 @@
+USE w3schools;
+
+# IN 하나라도 같으면...
+SELECT *
+FROM Customers
+WHERE Country = 'Germany'
+   OR Country = 'UK';
+
+SELECT *
+FROM Customers
+WHERE Country IN ('Germany', 'UK');
+
+SELECT *
+FROM Customers
+WHERE Country IN ('Germany', 'UK', 'Sweden', 'France');
+
+SELECT *
+FROM Products
+WHERE CategoryID IN (2, 3, 6);
+
+SELECT *
+FROM Suppliers
+WHERE Country IN ('Brazil', 'Japan', 'Italy');
