703cd20ad53fe8f3d78a96a175cb9ec60bfe2421
chaiheung <chaiheung@naver.com>
2024-04-23 15:01:23 +0900

    Controller31

--- src/main/java/com/study/controller/Controller31.java
+++ src/main/java/com/study/controller/Controller31.java
@@ -1,10 +1,15 @@
 package com.study.controller;
 
+import com.study.domain.MyBean25C;
+import com.study.domain.MyBean25E;
+import com.study.domain.MyBean25P;
 import com.study.mapper.Mapper02;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +22,73 @@ public class Controller31 {
     public void method1() {
         String now = mapper.selectNow();
         System.out.println("now = " + now);
+    }
+
+    @GetMapping("sub2")
+    public String method2() {
+        MyBean25C customer = mapper.selectOneCustomer();
+        System.out.println("customer = " + customer);
+
+        return null;
+    }
+
+    @GetMapping("sub3")
+    public void method3() {
+        MyBean25C c = mapper.selectOneCustomer2();
+        System.out.println("c = " + c);
+    }
+
+    @GetMapping("sub4")
+    public void method4() {
+        MyBean25C c = mapper.selectOneCustomer3();
+        System.out.println("c = " + c);
+    }
+
+    @GetMapping("sub5")
+    public void method5() {
+        MyBean25E e = mapper.selectOneEmployee1();
+        System.out.println("e = " + e);
+    }
+
+    @GetMapping("sub6")
+    public void method6() {
+        MyBean25E e = mapper.selectOneEmployee2(4);
+        System.out.println("e = " + e);
+    }
+
+    @GetMapping("sub7")
+    public void method7(Integer id) {
+        MyBean25E e = mapper.selectOneEmployee2(id);
+        System.out.println("e = " + e);
+    }
+
+    @GetMapping("sub8")
+    public void method8(Integer id) {
+        MyBean25C c = mapper.selectOneCustomer4(id);
+        System.out.println("c = " + c);
+    }
+
+    @GetMapping("sub9")
+    public void method9(Integer id) {
+        List<MyBean25C> customers = mapper.selectAllCustomers1();
+        System.out.println("customers = " + customers);
+    }
+
+    @GetMapping("sub10")
+    public void method10() {
+        List<MyBean25E> list = mapper.selectAllEmployees1();
+        list.forEach(System.out::println);
+    }
+
+    @GetMapping("sub11")
+    public void method11() {
+        List<MyBean25C> list = mapper.selectCustomersByCountry("usa", "uk");
+        list.forEach(System.out::println);
+    }
+
+    @GetMapping("sub12")
+    public void method12(Double min, Double max) {
+        List<MyBean25P> list = mapper.selectProductsByPriceBetween(min, max);
+        list.forEach(System.out::println);
     }
 }
--- src/main/java/com/study/mapper/Mapper02.java
+++ src/main/java/com/study/mapper/Mapper02.java
@@ -1,10 +1,126 @@
 package com.study.mapper;
 
+import com.study.domain.MyBean25C;
+import com.study.domain.MyBean25E;
+import com.study.domain.MyBean25P;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface Mapper02 {
     @Select("SELECT NOW()")
     public String selectNow();
+
+    // 조회 결과의 컬럼명과
+    // 리턴타입의 객체 프로퍼티명이 같으면
+    // 값을 바인딩 해줌
+    @Select("""
+            SELECT ContactName, Address, City, Country
+            FROM Customers
+            WHERE CustomerID = 1
+            """)
+    MyBean25C selectOneCustomer();
+
+    @Select("""
+            SELECT *
+            FROM Customers
+            WHERE CustomerID = 1
+            """)
+    MyBean25C selectOneCustomer2();
+
+    @Select("""
+            SELECT CustomerID id,
+                   CustomerName name,
+                   ContactName,
+                   Address,
+                   City,
+                   Country,
+                   PostalCode
+            FROM Customers
+            WHERE CustomerID = 1
+            """)
+    MyBean25C selectOneCustomer3();
+
+    @Select("""
+            SELECT EmployeeId id, FirstName, LastName, BirthDate, Photo, Notes
+            FROM Employees
+            WHERE EmployeeID = 2
+            """)
+    MyBean25E selectOneEmployee1();
+
+    @Select("""
+            SELECT EmployeeID id,
+                   LastName,
+                   FirstName,
+                   Photo,
+                   Notes,
+                   BirthDate
+            FROM Employees
+            WHERE EmployeeID = #{id}
+            """)
+    MyBean25E selectOneEmployee2(Integer id);
+
+    @Select("""
+            SELECT CustomerID id,
+                   CustomerName name,
+                   ContactName,
+                   Address,
+                   City,
+                   Country,
+                   PostalCode
+            FROM Customers
+            WHERE CustomerID = #{id}
+            """)
+    MyBean25C selectOneCustomer4(Integer id);
+
+
+    @Select("""
+            SELECT CustomerID id,
+                   CustomerName name,
+                   ContactName,
+                   Address,
+                   City,
+                   Country,
+                   PostalCode
+            FROM Customers
+            """)
+    List<MyBean25C> selectAllCustomers1();
+
+    @Select("""
+            SELECT EmployeeID id,
+                   LastName,
+                   FirstName,
+                   Photo,
+                   Notes,
+                   BirthDate
+            FROM Employees
+            ORDER BY BirthDate DESC
+            """)
+    List<MyBean25E> selectAllEmployees1();
+
+
+    @Select("""
+            SELECT CustomerID id,
+                   CustomerName name,
+                   ContactName,
+                   Address,
+                   City,
+                   Country,
+                   PostalCode
+            FROM Customers
+            WHERE Country = #{country1} 
+               OR Country = #{country2}
+            ORDER BY Country
+            """)
+    List<MyBean25C> selectCustomersByCountry(String country1, String country2);
+
+    @Select("""
+            SELECT ProductId id, ProductName name, SupplierId sid, CategoryId cid, Unit, Price
+            FROM Products
+            WHERE Price BETWEEN #{min} AND #{max}
+            ORDER BY Price
+            """)
+    List<MyBean25P> selectProductsByPriceBetween(double min, double max);
 }
