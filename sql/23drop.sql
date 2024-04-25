cde43d2481a9cadcfb7428e528a6357560f2609f
chaiheung <chaiheung@naver.com>
2024-04-25 11:58:27 +0900

    drop

---
+++ sql/23drop.sql
@@ -0,0 +1,22 @@
+USE w3schools;
+
+CREATE TABLE table99
+(
+    col1 INT
+);
+
+# DROP TABLE : 테이블 삭제
+# 주의해서 실행해야 하는 쿼리
+DROP TABLE table99;
+SELECT *
+FROM table99;
+
+# table100
+CREATE TABLE table100
+(
+    col1 INT
+);
+
+DROP TABLE table100;
+SELECT *
+FROM table100;
