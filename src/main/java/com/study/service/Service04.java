6b15b07b7b7db1aa798c9910a0dae85d732050d8
chaiheung <chaiheung@naver.com>
2024-04-30 12:47:47 +0900

    Controller37

---
+++ src/main/java/com/study/service/Service04.java
@@ -0,0 +1,48 @@
+package com.study.service;
+
+import com.study.domain.MyBean25E;
+import com.study.mapper.Mapper08;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
+public class Service04 {
+    private final Mapper08 mapper;
+
+    public void action1() {
+        mapper.query1();
+    }
+
+    public void action2() {
+        MyBean25E e = new MyBean25E();
+//        e.setLastName("흥민");
+        mapper.query2(e);
+    }
+
+    public void action3() {
+        MyBean25E e = new MyBean25E();
+        e.setFirstName("kim");
+        mapper.query3(e);
+    }
+
+    public void action4() {
+        MyBean25E e = new MyBean25E();
+
+//        e.setFirstName("kim");
+        e.setFirstName("lee");
+
+        mapper.query4(e);
+    }
+
+    public void action5() {
+        MyBean25E e = new MyBean25E();
+//        e.setLastName("son");
+        e.setFirstName("hm");
+        mapper.query5(e);
+    }
+
+
+}
