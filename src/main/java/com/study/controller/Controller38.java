2538599d4b6cc12050773ced0d972b1d5303e595
chaiheung <chaiheung@naver.com>
2024-04-30 16:43:30 +0900

    Controller38 - Authentication

--- src/main/java/com/study/controller/Controller38.java
+++ src/main/java/com/study/controller/Controller38.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,5 +9,29 @@ public class Controller38 {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/path1")
+    // 로그인 후 사용
+    // https://docs.spring.io/spring-security/reference/5.8/servlet/authorization/expression-based.html
+    @PreAuthorize("isAuthenticated()")
+    public void path1() {
+        System.out.println("Controller38.path1");
+    }
+
+    @GetMapping("/path2")
+    @PreAuthorize("hasAuthority('user')")
+    public void path2() {
+        System.out.println("사용자 권한으로 실행 가능");
+    }
+
+    @GetMapping("/path3")
+    @PreAuthorize("hasAuthority('admin')")
+    public void path3() {
+        System.out.println("관리자 권한으로 실행 가능");
+    }
+
+    @GetMapping("/path4")
+    public void path4() {
     }
 }
