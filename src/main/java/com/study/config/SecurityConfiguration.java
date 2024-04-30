a8555f5bdf00bfae3fe35a5fe4644b3f3d78020c
chaiheung <chaiheung@naver.com>
2024-04-30 16:08:01 +0900

    Controller38 - Security

---
+++ src/main/java/com/study/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.formLogin(form -> form.loginPage("/login"));
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
