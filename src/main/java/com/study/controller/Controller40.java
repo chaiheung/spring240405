953d8f436c17be4110c67b77a420cb0889912915
chaiheung <chaiheung@naver.com>
2024-05-16 10:04:47 +0900

    /api/someurl 의 get, post 요청 핸들러 메소드 작성

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -15,4 +15,16 @@ public class Controller40 {
     public void method2() {
         System.out.println("Controller40.method2");
     }
+
+    // /api/someurl2 연습
+
+    @GetMapping("/api/someurl2")
+    public void method3() {
+        System.out.println("Controller40.method3");
+    }
+
+    @PostMapping("/api/someurl2")
+    public void method4() {
+        System.out.println("Controller40.method4");
+    }
 }
